1a0f5f601ec58b9081b692eac9998f42fcb183f8
Sejun Park <66049273+pkt369@users.noreply.github.com>
2025-02-02 09:39:04 -0500

    Time: 41 ms (67.15%), Space: 46.1 MB (16.79%) - LeetHub

---
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minSessions(int[] tasks, int sessionTime) {
+        int n = tasks.length;
+        Pair[] dp = new Pair[1 << n];
+        Arrays.fill(dp, new Pair(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        dp[0] = new Pair(1, 0);
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) == 1) {
+                    continue;
+                }
+                int newMask = mask | (1 << i);
+                if (tasks[i] + dp[mask].used <= sessionTime) {
+                    dp[newMask] = minPair(dp[newMask], new Pair(dp[mask].session, tasks[i] + dp[mask].used));
+                } else {
+                    dp[newMask] = minPair(dp[newMask], new Pair(dp[mask].session + 1, tasks[i]));
+                }
+            }
+        }
+        return dp[(1 << n) - 1].session;
+    }
+
+    public Pair minPair(Pair a, Pair b) {
+        if (a.session > b.session) return b;
+        if (a.session < b.session) return a;
+        if (a.used < b.used) return a;
+        return b;
+    }
+
+    class Pair {
+        int session;
+        int used;
+        public Pair(int session, int used) {
+            this.session = session;
+            this.used = used;
+        }
+    }
+}
