e79724494d0741409c7e6b21d1c1630350f5c5a5
Sejun Park <66049273+pkt369@users.noreply.github.com>
2025-08-17 11:55:45 -0400

    Time: 141 ms (5.18%), Space: 61.7 MB (5.73%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,24 +1,25 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int len = speed.length;
-        List<Car> list = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-            list.add(new Car(position[i], speed[i], target));
+        List<Car> list = new LinkedList<>();
+        for (int i = 0; i < position.length; i++) {
+            Car car = new Car(position[i], speed[i], target);
+            list.add(car);
         }
-        Collections.sort(list);
 
-        int count = 0;
+        Collections.sort(list);
         Queue<Car> q = new LinkedList<>(list);
+        int count = 0;
         while (!q.isEmpty()) {
-            count++;
             Car standard = q.poll();
+            count++;
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 Car compare = q.poll();
-                if (standard.time >= compare.time) {
+                // System.out.println("standard: " + standard.time + ", compare: " + compare.time);
+                if (standard.time < compare.time) {
+                    q.offer(compare);
                     continue;
                 }
-                q.add(compare);
             }
         }
 
@@ -27,18 +28,19 @@ class Solution {
 }
 
 class Car implements Comparable<Car> {
-    int position;
+    int start;
     int speed;
     double time;
 
-    public Car (int position, int speed, int target) {
-        this.position = position;
+    public Car(int start, int speed, int target) {
+        this.start = start;
         this.speed = speed;
-        this.time = (double) (target - position) / speed;
+
+        time = (double)(target - start) / speed;
     }
 
     @Override
     public int compareTo(Car car) {
-        return car.position - this.position;
+        return car.start - this.start;
     }
 }
