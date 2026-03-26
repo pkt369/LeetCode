48f3c6eada7c3350ee9e590c6a3c029f4aa54d83
Sejun Park <66049273+pkt369@users.noreply.github.com>
2026-03-27 00:19:45 +0900

    Time: 3 ms (58.64%), Space: 47.2 MB (50.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,38 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        List<Number> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            list.add(new Number(nums[i], i));
-        }
-        Collections.sort(list);
-
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            if (list.get(left).value + list.get(right).value < target) {
-                left++;
-                continue;
+            int num = nums[i];
+            if (map.containsKey(target - num)) {
+                return new int[]{map.get(target - num), i};
             }
-            if (list.get(left).value + list.get(right).value > target) {
-                right--;
-                continue;
-            }
-            return new int[] {list.get(left).index, list.get(right).index};
+            map.put(num, i);
         }
-        return new int[]{};
-    }
-}
-
-class Number implements Comparable<Number> {
-    int value;
-    int index;
-
-    public Number(int value, int index) {
-        this.value = value;
-        this.index = index;
-    }
-
-    @Override
-    public int compareTo(Number num) {
-        return this.value - num.value;
+        return new int[] {0, 1};
     }
 }
