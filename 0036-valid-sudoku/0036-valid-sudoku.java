7f1623572d936397d0e5c51cf4419e0203e9f2da
Sejun Park <66049273+pkt369@users.noreply.github.com>
2026-03-27 20:19:21 +0900

    Time: 2 ms (66.52%), Space: 46.2 MB (94.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,33 +1,50 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<Set<Character>> rows = new ArrayList<>();
-        List<Set<Character>> columns = new ArrayList<>();
-        List<Set<Character>> squares = new ArrayList<>();
-
+        List<Set<Integer>> columns = new ArrayList<>();
+        List<Set<Integer>> boxes = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            rows.add(new HashSet<>());
             columns.add(new HashSet<>());
-            squares.add(new HashSet<>());
+            boxes.add(new HashSet<>());
         }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                char num = board[i][j];
-                if (num == '.') {
+        for (int i = 0; i < 9; i++) {
+            int[] rows = new int[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
                     continue;
                 }
-                if (rows.get(i).contains(num) || columns.get(j).contains(num)) {
-                    System.out.println(num);
+                int cell = board[i][j] - '0' - 1;
+                // System.out.println(cell);
+                rows[cell]++;
+                if (rows[cell] > 1) {
                     return false;
                 }
-                if (squares.get(((i / 3) * 3) + (j / 3)).contains(num)) {
+
+                Set<Integer> column = columns.get(j);
+                // System.out.print("몇번째: " + i + ", ");
+                // System.out.println(column.toString());
+                if (column.contains(cell)) {
                     return false;
                 }
-                rows.get(i).add(num);
-                columns.get(j).add(num);
-                squares.get(((i / 3) * 3) + (j / 3)).add(num);
+                column.add(cell);
+
+                Set<Integer> box = boxes.get((i / 3) * 3 + (j / 3));
+                if (box.contains(cell)) {
+                    return false;
+                }
+                box.add(cell);
             }
         }
         return true;
     }
 }
+
+// [".",".","4",".",".",".","6","3","."]
+// [".",".",".",".",".",".",".",".","."]
+// ["5",".",".",".",".",".",".","9","."]
+// [".",".",".","5","6",".",".",".","."]
+// ["4",".","3",".",".",".",".",".","1"]
+// [".",".",".","7",".",".",".",".","."]
+// [".",".",".","5",".",".",".",".","."]
+// [".",".",".",".",".",".",".",".","."]
+// [".",".",".",".",".",".",".",".","."]
