f6ec29129071c62a77fca0fb8e1a7c4228fdac91
Sejun Park <66049273+pkt369@users.noreply.github.com>
2026-03-29 15:53:14 +0900

    Time: 5 ms (82.8%), Space: 77.5 MB (21.43%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,16 +1,24 @@
+// (오른쪽 - 왼쪽) * min(height[오른쪽], height[왼쪽])
+// two pointer
+// 1. 왼쪽, 오른쪽 서로 끝에서 시작
+// 2. 더 작은쪽을 옮기기
+// 3. 계속 옮기기
+// 예상 시간 복잡도: O(n)
 class Solution {
     public int maxArea(int[] height) {
-        int ans = 0;
         int left = 0, right = height.length - 1;
+        int maxWater = 0;
         while (left < right) {
-            int min = Math.min(height[left], height[right]);
-            ans = Math.max(ans, min * (right - left));
-            if (height[left] > height[right]) {
-                right--;
-            } else {
+            int water = (right - left) * Math.min(height[left], height[right]);
+            // System.out.println("left: " + left + ", right: " + right + ", water: " + water);
+            maxWater = Math.max(maxWater, water);
+            
+            if (height[left] <= height[right]) {
                 left++;
+            } else {
+                right--;
             }
         }
-        return ans;
+        return maxWater;
     }
 }
