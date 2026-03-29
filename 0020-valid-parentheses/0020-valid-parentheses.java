f2c69d6d4de480a9b074895e13f836d53faf2759
Sejun Park <66049273+pkt369@users.noreply.github.com>
2026-03-29 16:20:25 +0900

    Time: 3 ms (87.35%), Space: 43.7 MB (13.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,40 +1,29 @@
+// key point: 열리는 괄호가 있으면 닫히는 괄호도 있어야 한다.
+// stack 을 통해 해결한다.
+// 짝은 map 으로 미리 정의해둔다.
+// 시간복잡도: O(n)
+// deque 에서 push = 앞에 넣기, pop = 앞에서 빼기, peek() = 앞보기
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Set<Character> set = getLeftSide();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (set.contains(c)) {
-                stack.add(c);
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.addLast(c);
                 continue;
             }
-            if (stack.isEmpty()) {
+            if (stack.size() == 0) {
                 return false;
             }
-            if (!isFair(stack.pop(), c)) {
+            if (stack.peekLast() != map.get(c)) {
                 return false;
             }
+            stack.removeLast();
         }
-        return stack.isEmpty();
-    }
-
-    private Set<Character> getLeftSide() {
-        Set<Character> set = new HashSet<>();
-        set.add('(');
-        set.add('[');
-        set.add('{');
-        return set;
-    }
-
-    private boolean isFair(char left, char right) {
-        Map<Character, Character> map = new HashMap<>();
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
-
-        if (!map.containsKey(left)) {
-            throw new RuntimeException("error");
-        }
-        return map.get(left) == right;
+        return stack.size() == 0;
     }
 }
